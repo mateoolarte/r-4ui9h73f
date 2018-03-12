ff766245758b2ebeeff90d86b859f3deedd984e5
Mateo Olarte <mateo.olarte8@gmail.com>
2018-03-12 01:15:16 -0500

    add repeat value of input

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      valinput: ""
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.repeatVal.bind(this)} />
+        <p className="repeater">{this.state.valinput}</p>
       </div>
     );
+  }
+
+  repeatVal(e) {
+    this.setState({
+      valinput: e.target.value
+    })
   }
 }
 
